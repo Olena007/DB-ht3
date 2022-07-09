c9033fa10abcb292fa63d5c9f13db968c88275de
lena <llogvinovaaaa@gmail.com>
2022-07-09 18:10:18 +0200

    codeFirst

---
+++ DB-codeFirst/DB-codeFirst/Confugurations/ProjectConfuguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DB_codeFirst.DbModels;
+
+namespace DB_codeFirst.Confugurations
+{
+    internal class ProjectConfuguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder
+                .ToTable("Project")
+                .HasKey(p => p.ProjectId);
+        }
+    }
+}
---
+++ DB-codeFirst/DB-codeFirst/DbModels/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_codeFirst.DbModels
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Budget { get; set; }
+
+        public DateTime Starteddate { get; set; }
+
+        public List<EmployeeProject> EmployeeProjects { get; set; } = new List<EmployeeProject>();
+    }
+}
